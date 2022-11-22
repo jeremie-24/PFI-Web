258d9380da568aef24d79a6c4038cb711c402a39
jeremie-24 <101289840+jeremie-24@users.noreply.github.com>
2022-11-22 10:00:03 -0500

    .,

--- wwwroot/ImagesManager2022 - Test/js/api.js
+++ wwwroot/ImagesManager2022 - Test/js/api.js
@@ -4,6 +4,7 @@ const tokenBaseURL = host + "/token";
 const accountsApiUrl = host + "/accounts";
 const registerUrl = accountsApiUrl + "/register";
 const verifyUrl = accountsApiUrl + "/verify";
+const userModifyUrl = accountsApiUrl + "/modify";
 const accountsGet = host + "/api/accounts/";
 
 function HEAD(successCallBack, errorCallBack) {
@@ -99,5 +100,17 @@ function VERIFY(successCallBack, errorCallBack, id, code) {
         type: 'GET',
         success: data => { successCallBack(data); },
         error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
+
+function MODIFY_USER(user, accessToken, successCallBack, errorCallBack){
+    $.ajax({
+        url: userModifyUrl + "/" + user.Id,
+        type: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify(user),
+        success: () => { successCallBack() },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) },
+        beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', accessToken ); }
     });
 }
