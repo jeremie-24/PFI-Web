918d1a5dbb83934e259c64367beb03cc2aa736ee
jeremie-24 <101289840+jeremie-24@users.noreply.github.com>
2022-12-09 11:42:56 -0500

    .

--- utilities.js
+++ utilities.js
@@ -10,7 +10,7 @@
 
 const queryStringParser = require('query-string');
 const tokens = require("./data/Tokens.json");
-const TokenManager = require("./tokenManager");
+
 
 exports.capitalizeFirstLetter = capitalizeFirstLetter;
 function capitalizeFirstLetter(s) {
@@ -30,7 +30,7 @@ exports.getAuthorization = (httpContext) => {
     return httpContext.req.headers["authorization"];
 }
 exports.getToken = (httpContext) => {
-    return TokenManager.find(exports.getAuthorization(httpContext));
+    return require("./tokenManager").find(exports.getAuthorization(httpContext));
 }
 exports.removeQueryString = removeQueryString;
 function removeQueryString(url) {
